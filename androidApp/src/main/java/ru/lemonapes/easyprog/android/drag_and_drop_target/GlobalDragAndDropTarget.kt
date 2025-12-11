fefbbadb798b9c683a51dce5bc9da3c90fd36931
alekse.pashkin <alekse.pashkin@x5.ru>
2025-12-11 22:36:43 +0400

    minor ui fix

--- androidApp/src/main/java/ru/lemonapes/easyprog/android/drag_and_drop_target/GlobalDragAndDropTarget.kt
+++ androidApp/src/main/java/ru/lemonapes/easyprog/android/drag_and_drop_target/GlobalDragAndDropTarget.kt
@@ -2,14 +2,21 @@ package ru.lemonapes.easyprog.android.drag_and_drop_target
 
 import androidx.compose.ui.draganddrop.DragAndDropEvent
 import androidx.compose.ui.draganddrop.DragAndDropTarget
+import androidx.compose.ui.draganddrop.toAndroidDragEvent
 import ru.lemonapes.easyprog.Utils.Companion.log
 import ru.lemonapes.easyprog.android.MainViewModel
 
 fun MainViewModel.createGlobalDragAndDropTarget(): DragAndDropTarget {
     return object : DragAndDropTarget {
         override fun onDrop(event: DragAndDropEvent): Boolean {
+            val label = event.toAndroidDragEvent()
+                .clipData
+                ?.description
+                ?.label
+                ?.toString()
+
             setItemIndexHovered(null)
-            return false
+            return label != "adding_item"
         }
 
         override fun onEntered(event: DragAndDropEvent) {
